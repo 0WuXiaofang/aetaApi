3eea2d570aca8a7c1d2d0e00f311087261b4b558
aFang <15342690689@139.com>
2022-05-02 17:09:39 +0800

    gevent

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from utils.app import create_app
+import time
 from flask_cors import CORS
-
+from gevent import monkey
+from gevent.pywsgi import WSGIServer
+monkey.patch_all()
 app = create_app()
 CORS(app, supports_credentials=True)
-
+@app.route('/')
+def index():
+    time.sleep(10)
+    return 'hello world'
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9999, debug=True)
-
+    http_server = WSGIServer(('0.0.0.0', 9999),app)
+    http_server.serve_forever()
